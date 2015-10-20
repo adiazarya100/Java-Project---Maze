fc324bcc99683858eb789b7e506df30373c01c13
zlil <zlil.korman@gmail.com>
2015-10-20 19:12:49 +0300

    new location -> package "boot"

---
+++ Mvp/src/boot/Run.java
@@ -0,0 +1,79 @@
+package boot;
+
+
+import generic.Enums;
+
+import java.beans.XMLDecoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Observable;
+
+import GUI.MazeWindow;
+import model.Model;
+import model.MyModel;
+import presenter.MyPresenter;
+import presenter.Presenter;
+import presenter.Properties;
+import view.MyView;
+import view.View;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Run.
+ */
+public class Run {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+	
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		Properties myProp = readProperties();
+		//--------כל מה שבירוק זה שינויים בשביל ה GUI, מה שהיה בהערה היה קודם--------//
+		//MyModel myModel = new MyModel(myProp); 
+		//MyView myView = new MyView(out, in); 
+		Model model = new MyModel(myProp);
+		View view = new MazeWindow("test", 500, 500);
+		
+		//Presenter presenter = new MyPresenter(myView, myModel);
+		Presenter presenter = new MyPresenter(view, model);
+		//myView.addObserver(presenter);
+		//myModel.addObserver(presenter);
+		
+		((Observable) view).addObserver(presenter);
+		((Observable) model).addObserver(presenter);
+		presenter.start();
+		
+		
+
+	}
+	
+	/**
+	 * Read preferences.
+	 * read propetries from XML file.
+	 * @return the properties
+	 */
+	public static Properties readProperties()
+	{
+		XMLDecoder d;
+		Properties p=null;
+		try {
+			BufferedInputStream in=new BufferedInputStream(new FileInputStream(Enums.XML_FILE_PATH));
+			d=new XMLDecoder(in);
+			p=(Properties)d.readObject();
+			System.out.println(p);
+			d.close();
+		} catch (IOException e) {
+			return new Properties();
+		}
+		return p;
+	}
+}
---
+++ Mvp/src/boot/Run2.java
@@ -0,0 +1,90 @@
+package boot;
+
+import generic.Enums;
+import generic.WritePropertiesGUI;
+
+import java.beans.XMLDecoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Observable;
+
+import model.Model;
+import model.MyModel;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import GUI.MazeWindow;
+import presenter.MyPresenter;
+import presenter.Properties;
+import view.MyView;
+import view.View;
+
+public class Run2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Display display=new Display();
+		Shell shell=new Shell(display);
+		WritePropertiesGUI guiProp=new WritePropertiesGUI();
+		guiProp.writeProperties(shell);
+		Model model;
+		View view;
+		Properties properties=readProperties();
+		if(properties!=null)
+		{
+			model=new MyModel(properties);
+			
+			switch(properties.getUi())
+			{
+				case CLI:
+					view=new MyView(new PrintWriter(System.out),new BufferedReader(new InputStreamReader(System.in)));
+					MyPresenter p=new MyPresenter(view,model);
+					((Observable) view).addObserver(p);
+					((Observable) model).addObserver(p);
+					view.start();
+					break;
+				case GUI:
+					
+					BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+					PrintWriter out = new PrintWriter(System.out);					
+					//Properties myProp = readProperties();
+					model = new MyModel(properties);
+					//view = new MazeWindow("test", 500, 500);
+					view=new MazeWindow(display, shell, "bobo", 1300, 700);
+					MyPresenter pMaze=new MyPresenter(view,model);
+					((Observable) view).addObserver(pMaze);
+					((Observable) model).addObserver(pMaze);
+					pMaze.start();
+					break;
+				default:
+					return;	
+			}
+		}
+		else
+			return;
+	
+	}
+
+	public static Properties readProperties()
+	{
+		XMLDecoder d;
+		Properties p=null;
+		try {
+			BufferedInputStream in=new BufferedInputStream(new FileInputStream(Enums.XML_FILE_PATH));
+			d=new XMLDecoder(in);
+			p=(Properties)d.readObject();
+			System.out.println(p);
+			d.close();
+		} catch (IOException e) {
+			return new Properties();
+		}
+		return p;
+	}
+}
